45508cbf4b4573f2144926fda95d313398ad1252
joseph <joseph.piron@gmail.com>
2015-06-10 02:59:49 +0200

    working

--- index.tpl
+++ index.tpl
@@ -1,26 +1,41 @@
 <html>
 <head>
-    <title>PiCam</title>
+    <title>PiTimelapse</title>
     <link rel="stylesheet" href="static/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <script>
+        function sendTime(){
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", "/setTime/" + new Date().toISOString(), false);
+            xmlHttp.send(null);
+            location.reload()
+        }
+    </script>
 </head>
 <body>
 <div class="container" style='margin-top:20px'>
-<p>
-    %if raspivid:
-        <a class='btn btn-success' href="rtsp://{{ip}}:9000/">Show the Preview!</a>
-    %else:
-        <a class='btn btn-warning' href="/video">Start Video</a>
-    %end
-</p>
-<p>
-    %if raspistill:
-        <a class='btn btn-danger' href="/stopTimelapse">Stop the timelapse</a>
-    %else:
-        <a class='btn btn-primary' href="/startTimelapse">Start the timelapse</a>
-    %end
-</p>
-    <img width=480 height="270" alt="Latest shot" src="/static/latest.jpg"/>
+    <p>
+    <span>
+        %if raspivid:
+            <a class='btn btn-success' href="rtsp://{{ip}}:9000/">Show the Preview!</a>
+        %else:
+            <a class='btn btn-warning' href="/video">Start Video</a>
+        %end
+    </span>
+    <span>
+        %if raspistill:
+            <a class='btn btn-danger' href="/stopTimelapse">Stop the timelapse</a>
+        %else:
+            <a class='btn btn-primary' href="/startTimelapse">Start the timelapse</a>
+        %end
+    </span>
+    </p>
+    <img width=480 height="270" alt="Latest shot" src="/static/latest.jpg">
+    <p>
+        Actual time: {{time}}
+        <a class='btn btn-small' href='javascript:sendTime()'>Set time from browser</a>
+        <span class="pull-right">{{usage}}</span>
+    </p>
 </div>
 </body>
 </html>
--- pitimelapse.py
+++ pitimelapse.py
@@ -4,35 +4,49 @@
 # Really handy to serve and configure raspberry + picam as timelapse camera
 
 from bottle import route, run, debug, template, redirect, os, time, static_file
+import subprocess
 
 
 @route('/')
 def index():
-    ip = os.popen('hostname -I').read().split()[0].strip()
-    raspivid = os.popen('pgrep raspivid').read() != ''
-    raspistill = os.popen('pgrep raspistill').read() != ''
-    return template('index', raspivid=raspivid, raspistill=raspistill, ip=ip)
+    values = dict(raspivid=os.popen('pgrep raspivid').read() != '')
+    values["raspistill"] = os.popen('pgrep raspistill').read() != ''
+    values["ip"] = os.popen('hostname -I').read().split()[0].strip()
+    values["time"] = time.asctime()
+    values["usage"] = subprocess.Popen(
+        ['df', '/media/sandisk'], stdout=subprocess.PIPE).communicate()[0].split('\n')[1].split()[4]
+    return template('index', **values)
+
 
 @route('/static/<path:path>')
 def latest(path):
-    print path
     return static_file(path, './')
 
+
 def startVideo():
-    os.system("raspivid -n -w 1280 -h 720 -b 4500000 -fps 30 -t 0 -o - | cvlc -I dummy -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:9000/}' :demux=h264 &")
+    os.system(
+        "sudo -u pi raspivid -n -w 1280 -h 720 -b 4500000 -fps 30 -t 0 -o - "
+        "|sudo -u pi vlc-wrapper -I dummy -vvv stream:///dev/stdin --sout "
+        "'#rtp{sdp=rtsp://:9000/}' :demux=h264 &")
+
 
 def stopVideo():
     os.system('killall vlc')
     os.system('killall raspivid')
 
+
 def startTimelapse():
     path = "/media/sandisk/timelapses/" + time.strftime('%Y-%m-%d')
     if not os.path.exists(path):
         os.mkdir(path)
-    os.system("raspistill -w 1920 -h 1080 -o " + path + r"/%05d.jpg -tl 5000 -t 84700000 -l latest.jpg &")
+    os.system("raspistill -w 1920 -h 1080 -o " + path +
+              r"/%05d.jpg -tl 5000 -t 84700000 -l "
+              "/home/pi/pitimelapse/latest.jpg &")
+
 
 def stopTimelapse():
     os.system('killall raspistill')
+
 
 @route('/video')
 def video():
@@ -40,16 +54,23 @@ def video():
     startVideo()
     redirect('/')
 
+
 @route('/startTimelapse')
 def start():
     stopVideo()
     startTimelapse()
     redirect('/')
 
+
 @route('/stopTimelapse')
 def stop():
     stopTimelapse()
     redirect('/')
+
+
+@route("/setTime/<newTime>")
+def setTime(newTime):
+    os.system("sudo date -s %s &" % newTime)
 
 debug(True)
 run(reloader=True, host="0.0.0.0", port=8000)
